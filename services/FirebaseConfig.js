30f1413eb6d615cc7c6c4952355c4f49c658d219
Laureano <laureanojuarez69@gmail.com>
2026-01-17 17:29:40 -0300

    feat: add Firebase configuration and environment variables

--- services/FirebaseConfig.js
+++ services/FirebaseConfig.js
@@ -1,22 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
-// Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBlzoQXLifJN6ucWeWBXJP9iCMukJY1Vuw",
-    authDomain: "are-you-ok-f9ef9.firebaseapp.com",
-    projectId: "are-you-ok-f9ef9",
-    storageBucket: "are-you-ok-f9ef9.firebasestorage.app",
-    messagingSenderId: "126599587793",
-    appId: "1:126599587793:web:d0efd55ec100f26fe7a2f0",
-    measurementId: "G-S9831LK7T9"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
